544eb17b6dac7728dbc745465f0ed5e502452745
Aminprhm <151400979+Aminprhm@users.noreply.github.com>
2024-03-18 13:31:57 +0330

    Update setImage.js

--- setImage.js
+++ setImage.js
@@ -1,6 +1,6 @@
 const newDiv = document.createElement("div");
 const img = document.createElement("img");
-img.src = "path/to/your/image.jpg";
+img.src = "<img src="https://latex.codecogs.com/png.image?\dpi{110}&space;a_{2}" title=" a_{2}" />";
 newDiv.appendChild(img);
 const container = document.getElementById("container");
 container.appendChild(newDiv);
