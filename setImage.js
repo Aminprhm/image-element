77c0230c35b408037ad588215516d43685681555
Aminprhm <151400979+Aminprhm@users.noreply.github.com>
2024-03-19 17:50:52 +0330

    Update setImage.js

--- setImage.js
+++ setImage.js
@@ -1,8 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-const newDiv = document.createElement("div");
-const img = document.createElement("img");
-img.src = "https://latex.codecogs.com/svg.image?a^{2}";
-newDiv.appendChild(img);
-const container = document.getElementById("form_description");
-container.appendChild(newDiv);
-})
+const containerId = "form_description";
+
+const observer = new MutationObserver(mutations => {
+  const container = document.getElementById(containerId);
+  if (container) {
+    const newDiv = document.createElement("div");
+    const img = document.createElement("img");
+    img.src = "https://latex.codecogs.com/svg.image?a^{2}";
+    newDiv.appendChild(img);
+    container.appendChild(newDiv);
+
+    observer.disconnect();
+  }
+});
+
+observer.observe(document.documentElement, { childList: true });
