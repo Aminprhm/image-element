6e6818c687009a482e0b5a0dd181a9a0bd418e14
Aminprhm <151400979+Aminprhm@users.noreply.github.com>
2024-03-19 18:11:38 +0330

    Update setImage.js

--- setImage.js
+++ setImage.js
@@ -3,6 +3,6 @@ const newDiv = document.createElement("div");
 const img = document.createElement("img");
 img.src = "https://latex.codecogs.com/svg.image?a^{2}";
 newDiv.appendChild(img);
-const container = document.getElementById("li_1");
+const container = document.getElementById("li_1").firstChild;
 container.appendChild(newDiv);
 })
